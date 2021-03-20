3c831991a31259e75bf2978db6018cfe96d6ac46
Rishabh <princerishabh.sharma400@gmail.com>
2021-03-20 09:57:47 +0530

    test: add failing test to check cd

--- tests/test.js
+++ tests/test.js
@@ -322,7 +322,7 @@ describe('AMURoboclub app DB Unit Testing', () => {
     const projectId = 'project_abc';
     const db = getFirestore(null);
     const testRead = db.collection('/projects').doc(projectId);
-    await firebase.assertSucceeds(testRead.get());
+    await firebase.assertFails(testRead.get());
   });
 
   it('PT -> Create Projects: {+} All Required Fields, {+} Admin Access, {+} Valid Field Types', async () => {
